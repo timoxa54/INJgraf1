e24802de488b7e1972f74786144959ff89bad257
Timofey <timoxa280254@gmail.com>
2022-04-29 18:44:42 +0500

    Шаг 2

--- INJgraf1/INJgraf1.cpp
+++ INJgraf1/INJgraf1.cpp
@@ -1,17 +1,43 @@
-﻿#include <GL/freeglut.h>
+﻿#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/vec3.hpp>
+using namespace glm;
+//#include "math_3d.h"
 
+GLuint VBO;
 
 static void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_POINTS, 0, 1);
+
+    glDisableVertexAttribArray(0);
+
     glutSwapBuffers();
 }
+
 
 static void InitializeGlutCallbacks()
 {
     glutDisplayFunc(RenderSceneCB);
 }
- 
+
+static void CreateVertexBuffer()
+{
+    vec3 Vertices[1];
+    Vertices[0] = vec3(0.0f, 0.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
 
 int main(int argc, char** argv)
 {
@@ -19,11 +45,20 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
     glutInitWindowSize(1024, 768);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 01");
+    glutCreateWindow("Tutorial 02");
 
     InitializeGlutCallbacks();
 
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateVertexBuffer();
 
     glutMainLoop();
 
